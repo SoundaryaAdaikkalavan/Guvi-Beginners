2b0e871892de4b430248bb6b0f655d9511dd78cf
SoundaryaAdaikkalavan <46521745+SoundaryaAdaikkalavan@users.noreply.github.com>
2019-01-09 12:09:18 +0000

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,13 @@
+n=str(input())
+if(n=="a" or n=="e" or n=="i" or n=="o" or n=="u"):
+    print("Vowel")n=str(input())
+if(n=="a" or n=="e" or n=="i" or n=="o" or n=="u"):
+    print("Vowel")
+elif(ord(n)>=97 and ord(n)<=122):
+    print("Consonant")
+else:
+    print("invalid")
+elif(ord(n)>=97 and ord(n)<=122):
+    print("Consonant")
+else:
+    print("invalid")

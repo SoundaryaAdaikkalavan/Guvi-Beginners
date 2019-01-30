433e70f9744c3f8532b182682c3d6d485ee37f2a
SoundaryaAdaikkalavan <46521745+SoundaryaAdaikkalavan@users.noreply.github.com>
2019-01-30 15:27:08 +0530

    Create compare char.py

---
+++ compare char.py
@@ -0,0 +1,13 @@
+a=input()
+b=input()
+c=0
+for i in range (0,len(a)):
+    for j in range(0,len(b)):
+        if(i==j):
+            if(a[i]!=b):
+                c=c+1
+if(c==1):
+    print("yes")
+        
+else:
+    print("no")
